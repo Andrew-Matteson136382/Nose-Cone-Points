b419d45ce49db7434e982acf7a3a61710f7b2e51
Andrew-Matteson136382 <acmwwp@umsystem.edu>
2022-09-13 22:09:04 -0500

    Rev 1, 9/13/2022

--- main.py
+++ main.py
@@ -5,26 +5,25 @@ import pandas as pd
 import datetime as date
 
 
+def von_karman(L, D, xarr):
+    yarr = np.empty(0)
+    zarr = np.zeros(len(xarr))
+    r = D / 2
+    phis = np.arccos(1 - (2 * xarr / L))
+    for i in phis:
+        y = (r / math.sqrt(math.pi)) * math.sqrt(i - ((math.sin(2 * i)) / 2))
+        yarr = np.append(yarr, np.array([y]))
+    arr = [xarr, yarr, zarr]
+    arr = np.transpose(arr)
+    f = open("von_karman_points.dat", 'w')
+    np.savetxt("von_karman_points.dat", arr)
+
+
 if __name__ == '__main__':
     L = 15  # Input length
     D = 3.12  # Input base diameter
-    res = 200  # resolution
+    res = 200  # Resolution of points, small = accruacy
 
-    xarr = np.linspace(0, L, res)
+    xarr = np.linspace(0, L, res)  # Generates length array
 
     von_karman(L, D, xarr)
-
-
-def von_karman(L, D, xarr):
-    yarr = np.empty(0)
-    r = D / 2
-    print(r)
-    for i in len(xarr):
-        phi = np.arccos(1 - (2 * xarr[i] / L))
-        y = (r / math.sqrt(math.pi)) * math.sqrt(phi - ((math.sin(2 * phi)) / 2))
-        yarr = np.append(yarr, np.array([y]))
-    f = open("von_karman", 'x')
-
-
-
-
